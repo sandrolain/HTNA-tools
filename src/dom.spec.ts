e636647e9a2c84cecd581f74039e43642a91cfe1
Sandro Lain <sandrolain@MBP-di-Sandro.station>
2020-05-26 18:37:29 +0200

    createElement tests

--- src/dom.spec.ts
+++ src/dom.spec.ts
@@ -1,15 +1,15 @@
-import { create } from "./dom";
+import { createElement } from "./dom";
 
 describe("dom", () => {
 
-  test("create() simple", async () => {
-    const node = create("div");
+  test("createElement() simple", async () => {
+    const node = createElement("div");
     expect(node).toBeInstanceOf(HTMLElement);
     expect(node.outerHTML).toEqual("<div></div>");
   });
 
-  test("create() with attributes", async () => {
-    const node = create("div", {
+  test("createElement() with attributes", async () => {
+    const node = createElement("div", {
       style: "border: 1px solid #FF0000; padding: 1em",
       title: "My new div element"
     });
@@ -17,23 +17,23 @@ describe("dom", () => {
     expect(node.outerHTML).toEqual(`<div style="border: 1px solid #FF0000; padding: 1em" title="My new div element"></div>`);
   });
 
-  test("create() with children", async () => {
-    const node = create("div", null, [
+  test("createElement() with children", async () => {
+    const node = createElement("div", null, [
       "Simple text and",
-      create("br"),
-      create("strong", null, ["Bold text"])
+      createElement("br"),
+      createElement("strong", null, ["Bold text"])
     ]);
     expect(node).toBeInstanceOf(HTMLElement);
     expect(node.outerHTML).toEqual(`<div>Simple text and<br><strong>Bold text</strong></div>`);
   });
 
-  test("create() with attributes and children", async () => {
-    const node = create("div", {
+  test("createElement() with attributes and children", async () => {
+    const node = createElement("div", {
       style: "border: 1px solid #FF0000; padding: 1em",
       title: "My new div element"
     }, [
       "Simple text and",
-      create("br"),
+      createElement("br"),
       ["strong", null, ["Bold text"]]
     ]);
     expect(node).toBeInstanceOf(HTMLElement);
