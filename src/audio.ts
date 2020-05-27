3f07309fd52918deec8a01f7e6c16514e443bb7d
Sandro Lain <sandrolain@MBP-di-Sandro.station>
2020-05-27 06:54:29 +0200

    Added AudioFromURL class

--- src/audio.ts
+++ src/audio.ts
@@ -18,10 +18,43 @@ export async function loadAudio (context: AudioContext, url: string): Promise<Au
 
 // TODO: test
 // TODO: docs
-export function playAudio (context: AudioContext, buffer: AudioBuffer): AudioBufferSourceNode {
+export function createAudioSource (context: AudioContext, buffer: AudioBuffer): AudioBufferSourceNode {
   const source = context.createBufferSource();
   source.buffer = buffer;
   source.connect(context.destination);
-  source.start(0);
   return source;
+}
+
+// TODO: test
+// TODO: docs
+export class AudioFromURL {
+  private audioBuffer: AudioBuffer;
+  private audioSource: AudioBufferSourceNode;
+
+  constructor (
+    private context: AudioContext,
+    public readonly url: string
+  ) {
+    this.load();
+  }
+
+  async load (): Promise<AudioBuffer> {
+    if(!this.audioBuffer) {
+      this.audioBuffer = await loadAudio(this.context, this.url);
+    }
+    return this.audioBuffer;
+  }
+
+  play (when: number = 0, offset: number = 0, duration?: number): void {
+    if(!this.audioSource) {
+      this.audioSource = createAudioSource(this.context, this.audioBuffer);
+    }
+    this.audioSource.start(when, offset, duration);
+  }
+
+  stop (): void {
+    if(this.audioSource) {
+      this.audioSource.stop();
+    }
+  }
 }
