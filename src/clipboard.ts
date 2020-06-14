24a96b2bb0157f3a95df6da43aa6df2c45a3ac90
Sandro Lain <sandrolain@MBP-di-Sandro.station>
2020-06-14 14:01:56 +0200

    Added use of Clipboard API to copyToClipboard()

--- src/clipboard.ts
+++ src/clipboard.ts
@@ -2,17 +2,20 @@
 // TODO: test
 // TODO: docs
 // TODO: string or dom ?
-export function copyToClipboard (str: string): boolean {
+export function copyToClipboard (text: string): Promise<void> {
+  if("clipboard" in navigator) {
+    return navigator.clipboard.writeText(text);
+  }
   const el = document.createElement("textarea");
   el.style.position = "absolute";
   el.style.left = "0";
   el.style.top = "0";
   el.style.opacity = "0";
-  el.value = str;
+  el.value = text;
   document.body.appendChild(el);
   el.focus();
   el.select();
   const result = document.execCommand("copy");
   document.body.removeChild(el);
-  return result;
+  return result ? Promise.resolve() : Promise.reject(new Error());
 }
